1e7cc5426733e9bcc7ae2b6e379acea4b74264b5
Ga <gabri.magni.97@gmail.com>
2022-03-15 14:49:04 +0100

    array tail adjustment

--- La coda dell' array/asset/js/main.js
+++ La coda dell' array/asset/js/main.js
@@ -1,9 +1,9 @@
-// let containeR = document.getElementById("container");
-// let mioArray = ["1", "2", "3", "4", "5", "6", "7", "8"];
+let containeR = document.getElementById("container");
+let mioArray = ["1", "2", "3", "4", "5", "6", "7", "8"];
 
-// for (i = mioArray.length - 5; i < mioArray.length; i++) {
-//     containeR.innerHTML += `<div>${mioArray[i]}</div>`
-// }
+for (i = mioArray.length - 5; i < mioArray.length; i++) {
+    containeR.innerHTML += `<div>${mioArray[i]}</div>`
+}
 
 let userChoice = parseInt(prompt("Qunti elementi deve contenere il tuo array?"));
 let exArray = [];
@@ -14,6 +14,16 @@ if (isNaN(userChoice)) {
 } else {
     for (i = 0; i < userChoice; i++) {
         exArray.push(Math.floor(Math.random() * 100) + 1)
+        containeR2.innerHTML += `<div>${exArray[i]}</div>`
+    }
+}
+
+let pullOutArrayElements = parseInt(prompt("Qunti elementi vuoi estrarre dal fondo del array?"));
+
+if (isNaN(pullOutArrayElements)) {
+    alert("ERRORE, ricaricare la pagina")
+} else {
+    for (i = 0; i < exArray.length - pullOutArrayElements; i++) {
         containeR2.innerHTML += `<div>${exArray[i]}</div>`
     }
 }
