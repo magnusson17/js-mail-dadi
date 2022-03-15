815d570ba58f922ea1a906ac6a2ed2f12f57de91
Ga <gabri.magni.97@gmail.com>
2022-03-15 13:43:45 +0100

    mail dreft js

--- Mail/asset/js/main.js
+++ Mail/asset/js/main.js
@@ -3,9 +3,11 @@ let containeR = document.getElementById("container");
 let mailList = ["ugo@gmail.com", "gio@gmail.com", "dan@gmail.com"];
 
 for (i = 0; i < mailList.length; i++) {
-    if (askMail == mailList[i]) {
+    if (mailList[i] == askMail) {
         containeR.innerHTML = `Benvenuto ${askMail}`
-    } else if (askMail != mailList[i]) {
+        console.log(askMail);
+    } else if (mailList[i] != askMail) {
         containeR.innerHTML = `Mail non registrata, prova ad usare un'altra mail`
+        console.log("no" + askMail);
     }
 }
