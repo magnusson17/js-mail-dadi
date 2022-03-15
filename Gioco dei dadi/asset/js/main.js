a9b289b4e729901084afeb63b02874d79bad7b90
Ga <gabri.magni.97@gmail.com>
2022-03-15 18:20:53 +0100

    gioco dadi draft

--- Gioco dei dadi/asset/js/main.js
+++ Gioco dei dadi/asset/js/main.js
@@ -1,15 +1,27 @@
 let containeR = document.getElementById("container");
-function gamble() {
-    let myChoice = Math.floor(Math.random() * 6) + 1;
-    let botChoice = Math.floor(Math.random() * 6) + 1;
+let myChoice;
+let container1ImgDiv = document.getElementById("container-1-img-div");
+let container2ImgDiv = document.getElementById("container-2-img-div");
 
-    if (myChoice > botChoice) {
-        containeR.innerHTML = `You win!`
-    } else if (myChoice < botChoice) {
-        containeR.innerHTML = `You lose!`
-    } else {
-        containeR.innerHTML = `Tie`
-    }
+function myGamble() {
+    myChoice = Math.floor(Math.random() * 6) + 1;
+    container1ImgDiv.innerHTML += `<img src:"../asset/img/dado${myChoice}.png" alt="dado">`
 }
 
 
+
+
+
+
+
+
+//     let botChoice = Math.floor(Math.random() * 6) + 1;
+
+//     if (myChoice > botChoice) {
+//         containeR.innerHTML = `You win!`
+//     } else if (myChoice < botChoice) {
+//         containeR.innerHTML = `You lose!`
+//     } else {
+//         containeR.innerHTML = `Tie`
+//     }
+// }
