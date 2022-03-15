9d8090db11d01b3aa2ab219568f11fc8834c6136
Ga <gabri.magni.97@gmail.com>
2022-03-15 17:48:41 +0100

    mail, general adjustment

--- Mail/asset/js/main.js
+++ Mail/asset/js/main.js
@@ -1,13 +1,16 @@
 let askMail = prompt("Inserisci la tua mail");
 let containeR = document.getElementById("container");
 let mailList = ["ugo@gmail.com", "gio@gmail.com", "dan@gmail.com"];
+let correctMail = false;
 
 for (i = 0; i < mailList.length; i++) {
     if (mailList[i] == askMail) {
-        containeR.innerHTML = `Benvenuto ${askMail}`
-        console.log(askMail);
-    } else if (mailList[i] != askMail) {
-        containeR.innerHTML = `Mail non registrata, prova ad usare un'altra mail`
-        console.log("no" + askMail);
+        correctMail = true;
     }
+}
+
+if (correctMail == true) {
+    containeR.innerHTML = `Benvenuto ${askMail}`
+} else {
+    containeR.innerHTML = `Mail non registrata, prova ad usare un'altra mail`
 }
