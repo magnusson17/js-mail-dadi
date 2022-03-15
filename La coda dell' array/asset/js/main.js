1c94e83b0403199791ec75e95485017356c2be89
Ga <gabri.magni.97@gmail.com>
2022-03-15 14:16:51 +0100

    array tail, 2nd part draft

--- La coda dell' array/asset/js/main.js
+++ La coda dell' array/asset/js/main.js
@@ -1,6 +1,15 @@
-let containeR = document.getElementById("container");
-let mioArray = ["1", "2", "3", "4", "5", "6", "7", "8"];
+// let containeR = document.getElementById("container");
+// let mioArray = ["1", "2", "3", "4", "5", "6", "7", "8"];
 
-for (i = mioArray.length - 5; i < mioArray.length; i++) {
+// for (i = mioArray.length - 5; i < mioArray.length; i++) {
+//     containeR.innerHTML += `<div>${mioArray[i]}</div>`
+// }
+
+let userChoice = parseInt(prompt("Qunti elementi deve contenere il tuo array?"));
+let exArray = [];
+let containeR2 = document.getElementById("container2");
+
+for (i = 0; i < userChoice; i++) {
+    containeR2.innerHTML += `<div></div>`
     containeR.innerHTML += `<div>${mioArray[i]}</div>`
 }
