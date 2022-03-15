4fe7d533beea99bf5a97a2453170af6e4e0d7e2e
Ga <gabri.magni.97@gmail.com>
2022-03-15 18:59:45 +0100

    general improvement

--- Gioco dei dadi/asset/js/main.js
+++ Gioco dei dadi/asset/js/main.js
@@ -1,27 +1,25 @@
 let containeR = document.getElementById("container");
 let myChoice;
+let botChoice;
 let container1ImgDiv = document.getElementById("container-1-img-div");
+let container2 = document.getElementById("container-2");
 let container2ImgDiv = document.getElementById("container-2-img-div");
+let container3 = document.getElementById("container-3");
 
 function myGamble() {
     myChoice = Math.floor(Math.random() * 6) + 1;
-    container1ImgDiv.innerHTML += `<img src:"../asset/img/dado${myChoice}.png" alt="dado">`
+    container1ImgDiv.innerHTML = `<img class="w_100px h_100px" src="./asset/img/dado${myChoice}.png">`;
+    container2.classList.remove("v_hidden");
 }
 
-
-
-
-
-
-
-
-//     let botChoice = Math.floor(Math.random() * 6) + 1;
-
-//     if (myChoice > botChoice) {
-//         containeR.innerHTML = `You win!`
-//     } else if (myChoice < botChoice) {
-//         containeR.innerHTML = `You lose!`
-//     } else {
-//         containeR.innerHTML = `Tie`
-//     }
-// }
+function botGamble() {
+    botChoice = Math.floor(Math.random() * 6) + 1;
+    container2ImgDiv.innerHTML = `<img class="w_100px h_100px" src="./asset/img/dado${botChoice}.png">`;
+    if (myChoice > botChoice) {
+        container3.innerHTML = `<p>You won!</p>`;
+    } else if (myChoice < botChoice) {
+        container3.innerHTML = `<p>You lost</p>`;
+    } else {
+        container3.innerHTML = `<p>It's a tie</p>`;
+    }
+}
